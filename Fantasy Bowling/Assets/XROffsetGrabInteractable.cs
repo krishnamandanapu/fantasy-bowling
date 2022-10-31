f94df205b01e46fbc58ca2bb8a59240943eec81c
unknown <gmorarey@gmu.edu>
2022-10-31 13:42:41 -0400

    movement, rotation, ray grab, menu prototypes

---
+++ Fantasy Bowling/Assets/XROffsetGrabInteractable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!attachTransform)
+        {
+            GameObject attachPoint = new GameObject("Offset Grab Pivot");
+            attachPoint.transform.SetParent(transform, false);
+            attachTransform = attachPoint.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
